50166c7f1bcc8469716fcfc67c537ef40db60d2c
Zongxing <zongxing.xie@stonybrook.edu>
2018-03-08 15:20:12 -0500

    pca

--- knn_py_breast-cancer/proj1_knn.py
+++ knn_py_breast-cancer/proj1_knn.py
@@ -9,13 +9,13 @@ df.drop(['id'], 1, inplace=True)
 X = np.array(df.drop(['diagno'],1))
 y = np.array(df['diagno'])
 
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(X,y,test_size=0.2)
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X,y,test_size=0.3)
 
 clf = neighbors.KNeighborsClassifier()
 clf.fit(X_train, y_train)
 
 accuracy = clf.score(X_test, y_test)
-print(accuracy)
+print('accuracy = ',accuracy)
 
 example_measures = np.array([[4,2,1,1,1,2,3,2,1,4,0,2,1,1,1,2,3,2,1,4,4,2,1,1,1,2,3,2,1,4]])
 example_measures = example_measures.reshape(len(example_measures),-1)
